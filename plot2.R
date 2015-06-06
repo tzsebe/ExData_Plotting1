9ed9eacef64467331220667f51ca4920c077398e
Tamas Zsebe <tzsebe@gmail.com>
2015-06-06 12:15:26 -0700

    Cleaning comment.

--- plot2.R
+++ plot2.R
@@ -1,7 +1,7 @@
 library(sqldf)
 library(lubridate)
 
-# Load the file and adjust the date type...
+# Load the file and add a DateTime to the DF.
 data <- read.csv.sql("household_power_consumption.txt", "select * from file where Date in ('1/2/2007', '2/2/2007')", header = TRUE, sep = ";")
 data$DateTime <- dmy_hms(paste(data$Date, data$Time))
 
