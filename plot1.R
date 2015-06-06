1f3d606143911f1858a1a11f6f32b36594db778c
Tamas Zsebe <tzsebe@gmail.com>
2015-06-06 11:51:09 -0700

    Removing date-related stuff, since it isn't needed on this one.

--- plot1.R
+++ plot1.R
@@ -1,7 +1,6 @@
-# Load the file and adjust the date type...
+# Load the file
 library(sqldf)
 data <- read.csv.sql("household_power_consumption.txt", "select * from file where Date in ('1/2/2007', '2/2/2007')", header = TRUE, sep = ";")
-data$Date <- as.Date(data$Date, "%d/%m/%Y")
 
 # Generate Plot
 png(filename = "plot1.png", width = 480, height = 480, bg = "transparent")
